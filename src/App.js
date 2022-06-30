946f6918017b70cd84ae222441a83a9012964e6e
Concha Reis <conchardr@gmail.com>
2022-06-30 09:53:37 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Dice from "./Dice";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
-function App() {
+export default function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+
+  const [tenzies, setTenzies] = React.useState(false);
+
+  React.useEffect(() => {
+    const allHeld = dice.every((dice) => dice.isHeld === true); // if any of the dice have isHeld as false, it will return false
+    // every one of them have to be true so it can return true
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((dice) => dice.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+    }
+    console.log("You won!");
+  }, [dice]); // second paramether because we only want this effect to run when the dice array changes in state
+
+  function generateNewDice() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
+  function allNewDice() {
+    //new array to hold my numbers
+    const newDice = [];
+    //loop 10 times
+    for (let i = 0; i < 10; i++) {
+      // push a random number from 1 to 6 to my array
+      // newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push(generateNewDice()); // and is now a object, and not a number
+    }
+    // return array
+    return newDice;
+  }
+
+  //  function rollDice() {
+  //   setDice(allNewDice());
+  // }
+
+  function rollDice() {
+    if (!tenzies) {
+      // only roll the dice if it does not have tenzies
+      setDice((oldDice) =>
+        oldDice.map((dice) => {
+          return dice.isHeld ? dice : generateNewDice();
+        })
+      );
+    } else {
+      setTenzies(false);
+      setDice(allNewDice());
+    }
+  }
+
+  function holdDice(id) {
+    // access to the oldDice (old array) to determinate what the new array should be
+    setDice((oldDice) =>
+      oldDice.map((dice) => {
+        // map will return a new array. Almost all dice element will be the same, except one of them wich will have the holdDice changed
+        // we will look into each dice element and use a ternary that will say
+        // if it is the same dice with the id property that was passed into the function
+        // than i will Update that object, and if not i will just return that dice
+        return dice.id === id
+          ? { ...dice, isHeld: !dice.isHeld } // if it will change, we take all properties and only change the isHeld
+          : // if it is not the same id, keep it as it was before
+            dice;
+      })
+    );
+  }
+
+  const diceElements = dice.map((dice) => (
+    <Dice
+      key={dice.id}
+      value={dice.value}
+      isHeld={dice.isHeld}
+      holdDice={() => holdDice(dice.id)} // indentifies which dice were clicked
+    />
+  ));
+  // look at each individual Dice and return a Dice component (<Dice/>) with the value prop with the value of that dice
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        {tenzies ? "New Game" : "Roll"}
+      </button>
+    </main>
   );
 }
 
-export default App;
+//EXTRA CREDITS
+// CSS: put real dots on the dice container
+// track the number of rolls
+// track the time it took to win
+// save your best time to localStorage
